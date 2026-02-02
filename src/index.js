d6023eda7a42466fb3fa699a2bb65b8db7f94a02
thatipallyvinodkumar167 <thatipallyvinodkumar167@gmail.com>
2026-02-02 19:32:41 +0530

    Fix index.html to use CRA auto-injection and add global CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import './index.css';
 import App from './App';
 import theme from './theme';
 import {BrowserRouter} from "react-router-dom"
